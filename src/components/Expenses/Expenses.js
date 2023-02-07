ad7aea832b452716325ebfc0afb62cdd19d9b6b4
Xing Ern <xingern28@hotmail.com>
2023-02-07 20:50:07 +0800

    add condition to display message when no expenses found

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,23 +9,26 @@ const Expenses = (props) => {
     const yearChangeHandler = (year) => {
         setFilteredYear(year)
     }
-
     const filteredExpenses = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear
     })
+    let expensesContent = <p>No expenses found.</p>
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date} 
+            />
+        ))
+    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onYearSelection={yearChangeHandler} />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date} 
-                    />
-                ))}
+                {expensesContent}
             </Card>
         </div>
 
